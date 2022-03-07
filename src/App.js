949d12f9fdd7c84e387624dadea065acc4a2d401
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-03-07 11:22:46 +1100

    use map function to refine codes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
 import SearchPage from "./SearchPage";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +16,7 @@ function App() {
           <Route path="/search" element={<SearchPage />} />
         </Routes>
         <Footer />
+  
       </BrowserRouter>
     </div>
   );
---
+++ src/DATA.js
@@ -0,0 +1,120 @@
+const DATA = {
+  category: [
+    {
+      img: "https://a0.muscache.com/im/pictures/0ff51b14-a9fb-4956-9807-b394bf68f68e.jpg?im_w=720",
+      title: "Wild adventures",
+      description:
+        "Experience Australian white sandy beaches, blue waters, and mountain tops.",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1601312044126-06d550c15beb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGlueSUyMGhvdXNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=720&q=60",
+      title: "Unique stays",
+      description: "Spaces that are more than just a place to sleep.",
+    },
+    {
+      img: "https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720",
+      title: "Entire homes",
+      description:
+        "Comfortable private places, with room for friends or family.",
+    },
+  ],
+  homeListings: [
+    {
+      img: "https://images.unsplash.com/photo-1600607686527-6fb886090705?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjJ8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=720&h=400&q=60",
+      title: "3 Bedroom townhouse in Windsor",
+      description:
+        "Superhost with a stunning view of the beachside in Sunny Windsor",
+      price: "$360/night",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1516455590571-18256e5bb9ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=720&h=400&q=60",
+      title: "Penthouse in Richmond",
+      description:
+        "Enjoy the amazing sights of Richmond with this stunning penthouse",
+      price: "$650/night",
+    },
+    {
+      img: "https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=720&h=400&q=60",
+      title: "1 Bedroom apartment in Southbank",
+      description:
+        "Superhost with great amenities and a fabolous shopping complex nearby",
+      price: "$220/night",
+    },
+  ],
+  searchListings: [
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU",
+      location: "Private room in center of London",
+      title: "Stay at this spacious Victorian House",
+      description:
+        "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+      star: 4.6,
+      price: "$120 / night",
+      total: "$530 total",
+    },
+
+    {
+      img: "https://www.expatkings.com/wp-content/uploads/2018/10/Airbnb-rental-tips.-Hostmaker-1-620x349.jpg",
+      location: "Private room in center of Melbourne CBD",
+      title: "Independant luxury studio apartment",
+      description:
+        "2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen",
+      star: 4.3,
+      price: "$140 / night",
+      total: "$610 total",
+    },
+
+    {
+      img: "https://www.smartertravel.com/uploads/2017/07/Untitled-design-8.jpg",
+      location: "Private room in center of Melbourne CBD",
+      title: "Melbourne 2 bedroom Apartments",
+      description:
+        "4 guest · 2 bedroom · 2 bed · 2 bathrooms · Free parking · Washing Machine",
+      star: 4.2,
+      price: "$135 / night",
+      total: "$570 total",
+    },
+    {
+      img: "https://cdn.bisnow.net/fit?height=489&type=jpeg&url=https%3A%2F%2Fs3.amazonaws.com%2Fcdn.bisnow.net%2Fcontent%2Fimages%2F2017%2F05%2F59151d0978bbf_https_press_atairbnb_com_app_uploads_2016_12_midtown_4.jpeg&width=717&sign=FeltIPi9cOWA36nVIeDvZxwgtiCZrpUyMRdvyZviTUI",
+      location: "PPrivate room in center of Melbourne CBD",
+      title: "5 mins walk to Flinder Street Station",
+      description:
+        "1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+      star: 3.1,
+      price: "$155 / night",
+      total: "$680 total",
+    },
+    {
+      img: "https://media.cntraveler.com/photos/5a8f258bd363c34048b35aac/master/w_2250,h_1500,c_limit/airbnb-plus-london.jpg",
+      location: "Private room in center of Melbourne CBD",
+      title: "Spacious Peaceful Modern Bedroom",
+      description:
+        "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning",
+      star: 4.9,
+      price: "$160 / night",
+      total: "$680 total",
+    },
+    {
+      img: "https://static.trip101.com/paragraph_media/pictures/001/676/061/large/969ae4bb-efd1-4fb9-a4e3-5cb3316dd3c9.jpg?1562227937",
+      location: "Private room in center of Melbourne CBD",
+      title: "The Blue Room In Carlton",
+      description:
+        "2 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Washing Machine",
+      star: 3.8,
+      price: "$165 / night",
+      total: "$690 total",
+    },
+    {
+      img: "https://image.insider.com/585029a0dd0895bc548b4b8b?width=750&format=jpeg&auto=webp",
+      location: "Private room in center of Melbourne CBD",
+      title: "5 Star Luxury Apartment",
+      description:
+        "3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine",
+      star: 4.7,
+      price: "$190 / night",
+      total: "$820 total",
+    },
+  ],
+};
+
+export default DATA;
