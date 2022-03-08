51c3391952bcefd285af78160748258bb71a20c7
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-03-09 01:37:54 +1100

    replace Card.js with mui cards components

--- src/Home.js
+++ src/Home.js
@@ -1,32 +1,59 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "./Navbar";
-import Card from "./Card";
-import { Link } from "react-router-dom";
+// import Card from "./Card";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 import DATA from "./DATA";
 
 const Home = () => {
   const allCategory = DATA.category.map((item, index) => (
-    <Link to="/search">
-    <Card
-      src={item.img}
-      title={item.title}
-      description={item.description}
-      key={index}
-    />
-    </Link>
+    <Card sx={{ maxWidth: 425 }} className="card">
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="300"
+        image={item.img}
+        alt=""
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item.description}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+  </Card>
   ));
 
   const homeListings = DATA.homeListings.map((item, index) => (
-    <Link to="/search">
-    <Card
-      src={item.img}
-      title={item.title}
-      description={item.description}
-      price={item.price}
-      key={index}
-    />
-    </Link>
+   
+     <Card sx={{ maxWidth: 425}} className="card">
+      <CardActionArea>
+        <CardMedia
+           component="img"
+        height="300"
+        image={item.img}
+        alt=""
+        />
+        <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item.description}
+          </Typography>
+          <Typography variant="h6" color="text.primary">
+          {item.price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   ));
 
   return (
@@ -39,3 +66,4 @@ const Home = () => {
 };
 
 export default Home;
+
