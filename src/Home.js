f241483565fc27444f617ece83ff4a72985c3f7a
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-03-09 01:45:16 +1100

    format Docs

--- src/Home.js
+++ src/Home.js
@@ -1,55 +1,43 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "./Navbar";
-// import Card from "./Card";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 import DATA from "./DATA";
 
 const Home = () => {
   const allCategory = DATA.category.map((item, index) => (
     <Card sx={{ maxWidth: 425 }} className="card">
-    <CardActionArea>
-      <CardMedia
-        component="img"
-        height="300"
-        image={item.img}
-        alt=""
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {item.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {item.description}
-        </Typography>
-      </CardContent>
-    </CardActionArea>
-  </Card>
+      <CardActionArea>
+        <CardMedia component="img" height="300" image={item.img} alt="" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   ));
 
   const homeListings = DATA.homeListings.map((item, index) => (
-   
-     <Card sx={{ maxWidth: 425}} className="card">
+    <Card sx={{ maxWidth: 425 }} className="card">
       <CardActionArea>
-        <CardMedia
-           component="img"
-        height="300"
-        image={item.img}
-        alt=""
-        />
+        <CardMedia component="img" height="300" image={item.img} alt="" />
         <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {item.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {item.description}
+          <Typography gutterBottom variant="h5" component="div">
+            {item.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.description}
           </Typography>
           <Typography variant="h6" color="text.primary">
-          {item.price}
+            {item.price}
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -66,4 +54,3 @@ const Home = () => {
 };
 
 export default Home;
-
