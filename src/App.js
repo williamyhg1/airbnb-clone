3d6632628a97ff138205a692b44839eec744845b
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-03-11 23:13:41 +1100

    commence auth

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./Header";
 import Footer from "./Footer";
 import SearchPage from "./SearchPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css';
 
 function App() {
   return (
