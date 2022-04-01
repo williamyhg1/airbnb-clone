172bab614c266fb1c687b106b46530cc508501c0
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-04-01 16:32:01 +1100

    add edit icon

--- src/AddedItems.js
+++ src/AddedItems.js
@@ -5,7 +5,8 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import { ref, onValue, remove } from "firebase/database";
 import { useState, useEffect } from "react";
@@ -24,38 +25,42 @@ function AddedItems() {
   const addedListings = Object.entries(listings).map(([key, data]) => (
     <Card sx={{ maxWidth: 400 }} key={key} className="card">
       {" "}
-      <CardActionArea>
-        <CardMedia component="img" height="300" image={data.img} alt="" />
-        <CardContent className="content">
+      <CardMedia component="img" height="300" image={data.img} alt="" />
+      <CardContent className="content">
+        <div>
+          <Typography variant="body2" color="text.secondary">
+            Property ID&nbsp;: &nbsp;{key}
+          </Typography>
+        </div>
+        <div>
+          <Typography gutterBottom variant="h5" component="div">
+            {data.title}
+          </Typography>
+        </div>
+        <div>
+          <Typography variant="body2" color="text.secondary">
+            {data.description}
+          </Typography>
+        </div>
+        <div className="delete">
+          <Typography variant="h6" color="text.primary">
+            ${data.price}/night
+          </Typography>
           <div>
-            <Typography variant="body2" color="text.secondary">
-              Property ID&nbsp;: &nbsp;{key}
-            </Typography>
-          </div>
-          <div>
-            <Typography gutterBottom variant="h5" component="div">
-              {data.title}
-            </Typography>
-          </div>
-          <div>
-            <Typography variant="body2" color="text.secondary">
-              {data.description}
-            </Typography>
-          </div>
-          <div className="delete">
-            <Typography variant="h6" color="text.primary">
-              ${data.price}/night
-            </Typography>
+            <IconButton>
+              <EditIcon />
+            </IconButton>
 
-            <DeleteOutlineOutlinedIcon
-              fontSize="medium"
+            <IconButton
               onClick={() => {
                 remove(ref(db, "Listings/" + key));
               }}
-            />
+            >
+              <DeleteOutlineOutlinedIcon fontSize="medium" />
+            </IconButton>
           </div>
-        </CardContent>
-      </CardActionArea>
+        </div>
+      </CardContent>
     </Card>
   ));
   return <>{addedListings}</>;
